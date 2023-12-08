8b0764e320bdfb3b6d844c23c1c16d93b67ad07f
Martin Wachtveitl <martin.wachtveitl@itewa.com>
2023-12-08 11:15:37 +0100

    quba-viewer-30 display order-x

--- app/config/app.config.js
+++ app/config/app.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     platform: process.platform,
     port: process.env.PORT ? process.env.PORT : 3000,
-    languages: ['en', 'de','fr'],
+    languages: ['en', 'de','fr', 'de.ids'],
     fallbackLng: 'en',
     namespace: 'translation'
   };
